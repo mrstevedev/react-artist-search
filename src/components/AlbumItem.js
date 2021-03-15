f1d11bc34edf6e031009782fb01cf5520db73ef6
Steven Polido <stevendotpulido@gmail.com>
2021-03-14 20:59:34 -0700

    Adjusted ternary for readability

--- src/components/AlbumItem.js
+++ src/components/AlbumItem.js
@@ -5,7 +5,9 @@ function AlbumItem({album, index}) {
     <Fragment>
       <div key={index} className="artist--search-results-row">
           <div className="artist--search-results-row-left">
-              <img alt="artist_album" src={album.strAlbumThumb !== null ? album.strAlbumThumb : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSADPzrYm_hQg2XMNc_9KTr9Axmn35s0DbsIQ&usqp=CAU'} width="50" height="50" />
+              <img alt="artist_album" src={album.strAlbumThumb !== null ? 
+                album.strAlbumThumb : 
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSADPzrYm_hQg2XMNc_9KTr9Axmn35s0DbsIQ&usqp=CAU'} width="50" height="50" />
         </div>
         <div className="artist--search-results-row-right">
             <h3>{album.strAlbum}</h3>
